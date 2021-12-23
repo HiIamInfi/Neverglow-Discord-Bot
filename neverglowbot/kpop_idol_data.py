44eba488a0aa5c253842e748057787aff2f8daf7
Bastian Wenzel <47057201+HiIamInfi@users.noreply.github.com>
2021-12-23 12:13:53 +0100

    added a script file for the kpop idol birthday shoutout extension

---
+++ neverglowbot/kpop_idol_data.py
@@ -0,0 +1,26 @@
+import discord
+from discord import client
+from discord.ext import commands
+from discord.ext.commands import bot
+from discord.ext.commands.cog import Cog
+from discord.ext.commands.core import command
+
+
+class Kpop_Idol_Data(commands.Cog):
+
+    def __init__(self, bot):
+        self.client = bot
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Extension KPop Idol Data loaded")
+
+    # Commands
+    @commands.command()
+    async def tester(self, ctx):
+        await ctx.send("Hi")
+
+
+def setup(bot):
+    bot.add_cog(Kpop_Idol_Data(bot))
