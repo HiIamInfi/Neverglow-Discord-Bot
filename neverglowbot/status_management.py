3c27da459d7f62c5a6af53c7e6620450e0c4506d
Bastian Wenzel <47057201+HiIamInfi@users.noreply.github.com>
2022-01-19 20:37:47 +0100

    added switching status to bot

--- neverglowbot/status_management.py
+++ neverglowbot/status_management.py
@@ -2,11 +2,11 @@ import discord
 from discord.ext import commands, tasks
 from random import choice
 
-PRESENCE_UPDATE_CYCLE = 3  # To be used as seconds
+PRESENCE_UPDATE_CYCLE = 600  # To be used as seconds
 
 
 def get_category():
-    list = ["playing", "watching", "listening", "streaming"]
+    list = ["playing", "watching", "listening"]
     return choice(list)
 
 
@@ -19,7 +19,9 @@ def get_watching():
         "Spiegel TV",
         "Wie man Rainbow Six Siege falsch spielt - Tutorial 2022",
         "The tiny, thicc Finger Report",
-        "Some Indian Guy on Youtube"
+        "Some Indian Guy on Youtube",
+        "Legally obtained Movies",
+        "Copyrighted Material with 80.000 viewers"
     ]
     return choice(list)
 
@@ -48,30 +50,38 @@ def get_playing():
     return choice(list)
 
 
-def get_streaming():
-    list = [
-        "Legally obtained Movies",
-        "Copyrighted Material for 80.000 viewers"
-    ]
-    return choice(list)
-
-
 class StatusManagement(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.update_presence.start()
 
     # Events
     @commands.Cog.listener()
     async def on_ready(self):
         print("Extension Status Management loaded")
+        await self.bot.change_presence(activity=discord.Game(get_playing()))
+        self.update_presence.start()
 
     # Tasks
 
     @tasks.loop(seconds=PRESENCE_UPDATE_CYCLE)
     async def update_presence(self):
 
-        print("Test Output")
+        activity = None
+        state = get_category()
+
+        if state == "playing":
+            print("PLAYING")
+            activity = discord.Game(name=get_playing())
+        elif state == "watching":
+            print("WATCHING")
+            activity = discord.Activity(
+                type=discord.ActivityType.watching, name=get_watching())
+        elif state == "listening":
+            print("LISTENING")
+            activity = discord.Activity(
+                type=discord.ActivityType.listening, name=get_listening())
+
+        await self.bot.change_presence(activity=activity, afk=False)
 
 
 def setup(bot: commands.Bot):
