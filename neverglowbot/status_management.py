9f3ac1e642b2bc4710da1bb29d2c579bf8dece47
Bastian Wenzel <47057201+HiIamInfi@users.noreply.github.com>
2022-01-19 16:58:34 +0100

    added aux methods to status management

--- neverglowbot/status_management.py
+++ neverglowbot/status_management.py
@@ -1,7 +1,59 @@
 import discord
 from discord.ext import commands, tasks
+from random import choice
 
 PRESENCE_UPDATE_CYCLE = 3  # To be used as seconds
+
+
+def get_category():
+    list = ["playing", "watching", "listening", "streaming"]
+    return choice(list)
+
+
+def get_watching():
+    list = [
+        "Itzy in Paris",
+        "Secret48",
+        "Everglowland",
+        "Anything but Agony’s Stream",
+        "Spiegel TV",
+        "Wie man Rainbow Six Siege falsch spielt - Tutorial 2022",
+        "The tiny, thicc Finger Report",
+        "Some Indian Guy on Youtube"
+    ]
+    return choice(list)
+
+
+def get_listening():
+    list = [
+        "Santiano",
+        "Jemand den man kennt",
+        "This one asian dude",
+        "Teddy Dandy",
+        "Everglow",
+        "ITZY",
+        "Twice"
+    ]
+    return choice(list)
+
+
+def get_playing():
+    list = [
+        "Spicy Noodle Cooking Simulator",
+        "Attorney Simulator",
+        "Schrauben, Sägen, Siegen - Das Spiel zur Serie",
+        "Monolith",
+        "Queen’s life"
+    ]
+    return choice(list)
+
+
+def get_streaming():
+    list = [
+        "Legally obtained Movies",
+        "Copyrighted Material for 80.000 viewers"
+    ]
+    return choice(list)
 
 
 class StatusManagement(commands.Cog):
@@ -18,6 +70,7 @@ class StatusManagement(commands.Cog):
 
     @tasks.loop(seconds=PRESENCE_UPDATE_CYCLE)
     async def update_presence(self):
+
         print("Test Output")
 
 
