7222af833200a06b9472d5d286f9ca6f417c7951
Bastian Wenzel <47057201+HiIamInfi@users.noreply.github.com>
2022-01-19 20:52:39 +0100

    Added a cog to take care of moderation

---
+++ neverglowbot/moderation.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+
+class Moderation(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name="clear")
+    @commands.has_permissions()
+    async def commandName(self, ctx: commands.Context, amount=1):
+        await ctx.channel.purge(limit=amount)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Moderation(bot))
