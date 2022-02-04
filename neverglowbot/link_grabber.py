0ec512597a22d1c559d7fc5c1b15ae796f22757c
Bastian Wenzel <47057201+HiIamInfi@users.noreply.github.com>
2022-02-04 20:46:24 +0100

    implemented event that checks for messages and sends a message with the content to a specific channel

--- neverglowbot/link_grabber.py
+++ neverglowbot/link_grabber.py
@@ -1,10 +1,26 @@
 import discord
 from discord.ext import commands
+from os import getenv
 
 
 class Link_Grabber(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.link_channel = int(getenv("LINK_CHANNEL"))
+
+    # Events
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Extension Link Grabber loaded")
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.author.bot:
+            return
+
+        channel = self.bot.get_channel(self.link_channel)
+        await channel.send(f"Message noticed in Link Grabber: {ctx.content}")
 
 
 def setup(bot: commands.Bot):
