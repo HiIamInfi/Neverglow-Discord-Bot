83b39dcef798f32bed0e5e336f358bf0d4d727c3
Bastian Wenzel <47057201+HiIamInfi@users.noreply.github.com>
2022-02-26 18:29:08 +0100

    Meme - got rid of references to the meme API, now targets the reddit API directly

--- neverglowbot/memes.py
+++ neverglowbot/memes.py
@@ -1,20 +1,60 @@
-from cgitb import text
+from turtle import title
 from nextcord.ext import commands, tasks
 import nextcord
 import os
 import json
 import requests
-from random import randint
+from random import randint, choice
+import praw
 
 
-def get_meme() -> str:
-    response = requests.get("https://reddit-meme-api.herokuapp.com/")
-    if response.status_code != 200:
-        print(f"Code {response.status_code}")
-        return "empty", "empty", "empty", "empty"
-    else:
-        meme_data = json.loads(response.content)
-        return meme_data["author"], meme_data["subreddit"], meme_data["title"], meme_data["url"]
+def scrape_meme(topic=0):
+    reddit = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_SECRET"),
+        user_agent=os.getenv("REDDIT_USER_AGENT"),
+        check_for_async=False
+    )
+
+    topics = [
+        "dankmemes",
+        "memes",
+        "AdviceAnimals",
+        "MemeEconomy",
+        "me_irl",
+        "ComedyCemetery",
+        "terriblefacebookmemes"
+    ]
+
+    if topic == 0:
+        topic = choice(topics)
+
+    subreddit = reddit.subreddit(topic)
+    meme = subreddit.random()
+    try:
+        _ = meme.preview
+        result = {
+            "post_link": meme.shortlink,
+            "subreddit": topic,
+            "title": meme.title,
+            "url": meme.url,
+            "author": meme.author.name,
+            "spoilers_enabled": subreddit.spoilers_enabled,
+            "nsfw": subreddit.over18,
+            "image_previews": [i["url"] for i in meme.preview.get("images")[0].get("resolutions")]
+        }
+    except Exception as e:
+        result = {
+            "post_link": meme.shortlink,
+            "subreddit": topic,
+            "title": meme.title,
+            "url": meme.url,
+            "author": meme.author.name,
+            "spoilers_enabled": subreddit.spoilers_enabled,
+            "nsfw": subreddit.over18,
+            "image_previews": ["No Preview Found For This Meme.. Sorry For That"]
+        }
+    return result
 
 
 class Memes(commands.Cog):
@@ -31,7 +71,13 @@ class Memes(commands.Cog):
     # Commands
     @commands.command(name="meme", brief="Fetches a meme")
     async def meme(self, ctx: commands.Context):
-        author, subreddit, title, url = get_meme()
+
+        res = scrape_meme()
+
+        title = res["title"]
+        url = res["url"]
+        subreddit = res["subreddit"]
+        author = res["author"]
 
         embed = nextcord.Embed(
             title=title, colour=nextcord.Colour.from_rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
@@ -41,14 +87,16 @@ class Memes(commands.Cog):
         await ctx.send(embed=embed)
 
     # Tasks
-    @tasks.loop(seconds=300)  # To be changed to 3600
+    @tasks.loop(seconds=3600)  # To be changed to 3600
     async def hourly_meme(self):
         channel = self.bot.get_channel(self.meme_channel)
 
-        author, subreddit, title, url = get_meme()
+        res = scrape_meme()
 
-        if author == "empty":
-            return
+        title = res["title"]
+        url = res["url"]
+        subreddit = res["subreddit"]
+        author = res["author"]
 
         embed = nextcord.Embed(
             title=title, colour=nextcord.Colour.from_rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
