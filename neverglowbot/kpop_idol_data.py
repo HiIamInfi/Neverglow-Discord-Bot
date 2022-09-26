6dbd11300ba66a90f9e93cda50d33e0ae816e0f4
Bastian Wenzel <47057201+HiIamInfi@users.noreply.github.com>
2022-09-26 19:24:04 +0200

    Added Idol dataclass and adjusted present functionality accordingly

--- neverglowbot/kpop_idol_data.py
+++ neverglowbot/kpop_idol_data.py
@@ -1,156 +1,154 @@
 import datetime
 import os
+from dataclasses import asdict, astuple, dataclass
 from json import load
 
-from nextcord.ext import commands, tasks
 from nextcord import Colour, Embed
+from nextcord.ext import commands, tasks
+
+# Auxiliary class
+
+
+@dataclass(frozen=False, order=True)
+class Idol():
+    id: str         # Unique ID
+    name: str       # Legal Name
+    birthday: datetime.datetime   # Birthday
+    group: str      # Group name
+    country: str    # Country of birth
+    stage_name: str  # Stage name
+    image_url: str  # URL to image on imgur
+    age: int  # Age
+    diff_next: int  # Days left till next birthday
+
+    def __init__(self, id: str, name: str, birthday: str, group: str, country: str, stage_name: str, image_url: str):
+        self.id = id
+        self.name = name
+        self.birthday = datetime.datetime.strptime(birthday, "%d.%m.%Y")
+        self.group = group
+        self.country = country
+        self.stage_name = stage_name
+        self.image_url = image_url
+        self.age = datetime.datetime.today().year - self.birthday.year - \
+            ((datetime.datetime.today().month, datetime.datetime.today().day)
+             < (self.birthday.month, self.birthday.day))
+        this_year = self.birthday \
+            .replace(year=datetime.datetime.today().year)
+        if this_year.date() < datetime.datetime.today().date():
+            this_year = this_year.replace(
+                year=datetime.datetime.today().year+1)
+        self.diff_next = abs(this_year.date() -
+                             datetime.datetime.today().date()).days
+
+    def update(self):
+        # Update age
+        today = datetime.today()
+        idol_birthday = datetime.strptime(self.birthday, "%d.%m.%Y")
+        idol_birthday = idol_birthday.replace(year=today.year)
+        if idol_birthday < today:
+            idol_birthday = idol_birthday.replace(year=today.year+1)
+        self.diff_next = abs(idol_birthday - today).days
+        # Update days till next birthday
+        today = datetime.today()
+        born = datetime.strptime(self.birthday, "%d.%m.%Y")
+        self.age = today.year - born.year - \
+            ((today.month, today.day) < (born.month, born.day))
 
 # Auxiliary Methods
 
 
-def is_birthday(birthday: str) -> bool:
-    datestring = datetime.date.today().strftime("%d.%m.%Y")
-    if datestring[:6] == birthday[:6]:
-        return True
-    else:
-        return False
-
-
-def calc_difference(idol_birtday: str) -> int:
-    today = datetime.date.today()
-    idol_birtday = idol_birtday.split(".")
-
-    idol_birtday = datetime.date(today.year, int(
-        idol_birtday[1]), int(idol_birtday[0]))
-
-    if idol_birtday < today:
-        idol_birtday = idol_birtday.replace(year=today.year+1)
-
-    return abs(idol_birtday - today).days
-
-
-def get_deltas(idol_dataset: list) -> list:
-    deltas = []
-    for idol in idol_dataset:
-        new_line = (idol["name"], idol["birthday"],
-                    calc_difference(idol["birthday"]), idol["image_url"])
-        deltas.append(new_line)
-
-    deltas.sort(key=lambda x: x[2], reverse=False)
-
-    return deltas
-
-
-def get_current_hour() -> str:
-    now = datetime.datetime.now()
-    return now.strftime("%H")
-
-
-def has_been_run_today() -> bool:
-    file = f"{os.getcwd()}/neverglowbot/resources/last_check.txt"
-    with open(file, "r") as f:
-        last_check = f.read()
-        last_check = last_check.split(sep=",")
-    if last_check[0] == datetime.date.today().strftime("%d.%m.%Y"):
-        return True
-    else:
-        return False
-
-
-def update_last_check() -> None:
-    file = f"{os.getcwd()}/neverglowbot/resources/last_check.txt"
-    with open(file, "w") as f:
-        f.write(datetime.datetime.now().strftime("%d.%m.%Y,%H:%M:%S"))
-
-
-def get_keys() -> list:
-    with open(os.getcwd()+"/neverglowbot/resources/idol_data.json", "r") as read_file:
-        keys = load(read_file)
-    return keys
-
-
-def get_idol(idol: str) -> dict:
-    with open(os.getcwd()+"/neverglowbot/resources/idol_data.json", "r") as read_file:
-        idol_data = load(read_file)
-    return idol_data[idol]
-
-
-def get_idol_data(keys: list) -> list:
+def get_idol_data(path: str) -> list:
+    # Aquire keys
+    with open(path, "r") as read_file:
+        raw_data = load(read_file)
+    # Initialize the list
     idol_data = []
-
-    for key in keys:
-        new_idol = get_idol(key)
+    # Create a new Idol object for each idol in idol_data.json
+    for key in raw_data:
+        new_idol = get_idol(path, key)
         idol_data.append(new_idol)
 
     return idol_data
+
+
+def get_idol(path: str, idol_id: str) -> Idol:
+    # Aquire raw data
+    with open(path, "r") as read_file:
+        idol_data = load(read_file)
+    ask = idol_data[idol_id]
+    tell = Idol(id=idol_id, name=ask["name"],
+                birthday=ask["birthday"], group=ask["group"], country=ask["country"], stage_name=ask["stage_name"], image_url=ask["image_url"])
+    return tell
 
 
 class Kpop_Idol_Data(commands.Cog):
 
     def __init__(self, bot):
         self.client = bot
+        cpath = os.getenv("CONFIG")
+        self.idol_data = get_idol_data(f"{cpath}idol_data.json")
         self.idol_channel = int(os.getenv("IDOL_ALERT_CHANNEL"))
+        self.run = datetime.datetime(1900, 1, 1)
 
     # Events
     @commands.Cog.listener()
     async def on_ready(self):
         print("Extension KPop Idol Data loaded")
-        self.idol_birtday_shoutout.start()
+        self.idol_birthday_shoutout.start()
 
     # Commands
     @commands.command(name="nextbday", brief="Ouputs the next Idols birthday and also names the following four")
     async def k_forecast(self, ctx):
-        data = get_idol_data(get_keys())
-        delta_list = get_deltas(data)
 
-        forecast_data = delta_list[0:5]
-        print(forecast_data)
+        forecast_data = sorted(
+            self.idol_data, key=lambda x: x.diff_next, reverse=False)
 
         embed = Embed(
-            title=f"Thank you for asking! Next up is {forecast_data[0][0]}",
+            title=f"Thank you for asking! Next up is {forecast_data[0].name}",
             colour=Colour.from_rgb(141, 106, 159),
             type="rich",
-            description=f"The next Idols birthday will be {forecast_data[0][0]}, her birthday is on {forecast_data[0][1]}, in {forecast_data[0][2]} days. \n Next after that: {forecast_data[1][0]} in {forecast_data[1][2]} days, {forecast_data[2][0]} in {forecast_data[2][2]} days, {forecast_data[3][0]} in {forecast_data[3][2]} days, {forecast_data[4][0]} in {forecast_data[4][2]} days."
+            description=f"The next Idols birthday will be {forecast_data[0].name}, her upcoming birthday is in {forecast_data[0].diff_next} days. \n Next after that: {forecast_data[1].name}, her upcoming birthday is in {forecast_data[1].diff_next} days."
         )
-        embed.set_thumbnail(url=forecast_data[0][3])
+        embed.set_thumbnail(url=forecast_data[0].image_url)
         await ctx.send(embed=embed)
 
     # Tasks
 
     @tasks.loop(seconds=20)
-    async def idol_birtday_shoutout(self):
-        # Check if current time is between 01:00 am and 01:59 am
-        if int(get_current_hour()) != 1:
-            return
+    async def idol_birthday_shoutout(self):
+        # Check if current time is between 00:00 am and 00:59 am
+        # if int(datetime.datetime.now().strftime("%H")) != 0:
+        #    return
         # Check if task was already completed today
-        if has_been_run_today():
+        if self.run.date() == datetime.datetime.now().date():
             return
         # Get Data from CSV
-        data = get_idol_data()
         cache = []
         # Search for positive Hits
-        for idol in data:
-            if is_birthday(idol["birthday"]):
+        for idol in self.idol_data:
+            if idol.diff_next == 0:
                 cache.append(idol)
+
+        print(cache)
         # Prepare & send messages
         for element in cache:
             try:
                 # Calc age based on current day
                 calc_age = int(datetime.date.today().strftime("%Y")) - \
-                    int(element["birthday"][6:])
+                    int(element.birthday.strftime("%Y"))
                 # Create Embed
                 embed = Embed(
-                    title=f"Happy { element['stage_name'] } Day!",
+                    title=f"Happy { element.stage_name } Day!",
                     colour=Colour.from_rgb(141, 106, 159),
                     type="rich",
-                    description=f"Today is {element['group']}'s {element['stage_name']}'s Birtday!\nShe got {calc_age} years old today!")
+                    description=f"Today is {element.group}'s {element.stage_name}'s Birthday!\nShe got {calc_age} years old today!")
                 embed.set_thumbnail(
-                    url=element["image_url"])
+                    url=element.image_url)
                 channel = self.client.get_channel(self.idol_channel)
                 # Send message
                 await channel.send(embed=embed)
                 # Update last check
-                update_last_check()
+                self.run = datetime.datetime.now()
             except Exception as e:
                 print(e)
 
